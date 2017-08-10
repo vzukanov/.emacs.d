cc593b4881b5ed360fb0bfe7e76ab9b184912972
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2017-08-10 17:39:59 +0300

    Adding web mode for web-related files

--- init.el
+++ init.el
@@ -38,8 +38,8 @@
 ;; Load verilog mode only when needed
 (autoload 'verilog-mode "verilog-mode" "Verilog mode" t )
 
-;; Load nXhtml mode only when needed
-(autoload 'nxhtml-mode "nxhtml-mode" "nXhtml mode" t )
+;; Load web mode only when needed
+(autoload 'web-mode "web-mode" "web mode" t )
 
 ;; Load Yaml mode only when needed
 (autoload 'yaml-mode "yaml-mode" "yaml mode" t)
@@ -86,12 +86,21 @@
 ;; Also .upf files in tcl  mode
 (add-to-list 'auto-mode-alist '("\\.upf\\'" . tcl-mode))
 
-;; PHP files in xNhtml mode
-(add-to-list 'auto-mode-alist '("\\.php\\'" . nxhtml-mode))
-
 ;; yml in Yaml mode
 (add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . yaml-mode))
 
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.s?css\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.xml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.hbs\\'" . web-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; HOOKS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
