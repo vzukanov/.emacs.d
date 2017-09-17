1c686842ca913e2410dbfdc7ea2802ac5b846235
Vasiliy Zukanov <vasiliy.zukanov@gmail.com>
2017-09-17 11:25:04 +0300

    Fixing a bug that prevented php files being shown in web-mode

--- init.el
+++ init.el
@@ -90,7 +90,7 @@
 (add-to-list 'auto-mode-alist '("\\.ya?ml\\'" . yaml-mode))
 
 (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
